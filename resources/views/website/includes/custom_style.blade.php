7852feaca1c19e6c66729c0b8d01a140d0ac75db
Techyfo <nurnobishanto1@gmail.com>
2025-08-09 02:35:45 +0600

    update custom scripting

--- resources/views/website/includes/custom_style.blade.php
+++ resources/views/website/includes/custom_style.blade.php
@@ -1,17 +1,1 @@
-<style>
-    .exam-name {
-        display: -webkit-box;
-        -webkit-line-clamp: 2;
-        -webkit-box-orient: vertical;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        transition: all 0.3s ease;
-    }
 
-    .exam-name:hover {
-        -webkit-line-clamp: unset;
-        overflow: visible;
-        white-space: normal;
-    }
-
-</style>
