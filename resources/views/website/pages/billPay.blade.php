ec89a74e9e43fe72b7732e64aef6433e8d284e51
Techyfo <nurnobishanto1@gmail.com>
2025-08-09 03:34:12 +0600

    Update billPay.blade.php

--- resources/views/website/pages/billPay.blade.php
+++ resources/views/website/pages/billPay.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.master')
 
 @section('content')
-    <!-- Start Page Title Area -->
-    @include('website.partials.page_header',['title'=>'Pay Your IP Number Due Bill'])
+
     <!-- End Page Title Area -->
     <section class="ptb-100">
         <div class="container">
+            <h2 class="text-center">Pay Your IP Number Due Bill</h2>
             <div class="row justify-content-center align-items-center">
                 <div class="col-12 col-md-6">
                     <form action="" method="GET" class="p-4 shadow rounded bg-white">
