c015c3efe44bb9269b9c3dd5727b24f9014a911c
Techyfo <nurnobishanto1@gmail.com>
2025-08-08 15:30:08 +0600

    setting

--- app/Helpers/global.php
+++ app/Helpers/global.php
@@ -1,1 +1,27 @@
 <?php
+use App\Models\Setting;
+
+if (!function_exists('getSetting')) {
+    function getSetting(string $key, mixed $default = null): mixed
+    {
+        static $cache = [];
+
+        if (array_key_exists($key, $cache)) {
+            return $cache[$key];
+        }
+
+        $setting = Setting::where('key', $key)->first();
+
+        return $cache[$key] = $setting ? $setting->value : $default;
+    }
+}
+
+if (!function_exists('setSetting')) {
+    function setSetting(string $key, mixed $value): bool
+    {
+        return (bool) Setting::updateOrCreate(
+            ['key' => $key],
+            ['value' => $value]
+        );
+    }
+}
